176a5f7a2b7e163594e3776707c92ec74189e171
Keszi94 <keszi94@gmail.com>
2025-04-13 23:16:32 +0100

    Fixing the image field part 1

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 
@@ -8,16 +9,22 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            categories = Category.objects.all()
-            # list comprehension -
-            # - shorthand way of for loop to add items to a list
-            friendly_names = [
-                (c.id, c.get_friendly_name()) for c in categories
-            ]
+    image = forms.ImageField(
+        label='Image',
+        required=False,
+        widget=CustomClearableFileInput
+        )
 
-            # instead of cat.id or name we will see the friendly name
-            self.fields['caregory'].choices = friendly_names
-            for field_name, field in self.fields.items():
-                field.widget.attrs['class'] = 'border-black rounded-0'
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        # list comprehension -
+        # - shorthand way of for loop to add items to a list
+        friendly_names = [
+            (c.id, c.get_friendly_name()) for c in categories
+        ]
+
+        # instead of cat.id or name we will see the friendly name
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
